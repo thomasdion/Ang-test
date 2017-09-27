0ffdfac9eff3bf23bb4ec7e9b50e3044e0f38ecc
thomasdion <thomas.dionisis81@gmail.com>
2017-09-27 09:45:46 +0300

    Adding Links to Site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { RouterModule, Routes} from '@angular/router';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 
+const appRouting : Routes =[
+ {path:'servers', component:ServersComponent }
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +17,8 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule,
+    RouterModule.forRoot(appRouting, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
